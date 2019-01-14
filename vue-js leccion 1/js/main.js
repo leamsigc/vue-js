f3f4cddd56c1726e5645e4822bdef14421baf6d1
leamsigc <leamsigc@hotmail.com>
2019-01-13 23:36:10 -0500

    delete the leccion one

--- vue-js leccion 1/js/main.js
+++
@@ -1,9 +0,0 @@
-// // vainilla java script
-// const input = document.querySelector('input');
-// const h1 =document.querySelector('h1');
- 
-// input.addEventListener('keyup', function(){
-//     h1.innerHTML = input.value;
-
-// });
-// // El problema se complica al entender y mantener en futuro
